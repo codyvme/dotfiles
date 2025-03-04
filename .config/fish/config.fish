7bad14512e6dbb254870e147f83646ebb4e6c032
Cody VerKuilen <clverkuilen@gmail.com>
2025-03-03 20:42:34 -0500

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set -g fish_greeting
 
+bind tab accept-autosuggestion
+
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias ls lsd
 alias cat 'bat --theme="Catppuccin Latte"'
