36be4cdbf1c11abe25fa456f0b1193c6ae39cca0
Cody VerKuilen <clverkuilen@gmail.com>
2025-04-07 11:15:00 -0400

    adding logic to only start up the ssh-agent if needed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,11 @@ alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias ls lsd
 alias cat 'bat --theme="Catppuccin Latte"'
 
-eval (ssh-agent -c)
-ssh-add
+if not pgrep -u "$USER" ssh-agent > /dev/null
+    eval (ssh-agent -c) > /dev/null 2>&1
+    ssh-add > /dev/null 2>&1
+else
+    ssh-add -l > /dev/null || ssh-add > /dev/null 2>&1
+end
 
 starship init fish | source
